073989ea8fb6c66085d37d41f679b41966dc9704
DF <dragonflybaby2.0@gmail.com>
2024-02-13 16:32:27 +0800

    Update: Enhance Character class and project configuration - Updated Character class in Character.hpp and implemented methods in Character.cpp for improved functionality - Added compiler options and compile definitions in CMakeLists.txt for MSVC and non-MSVC compilers

---
+++ include/Character.hpp
@@ -0,0 +1,43 @@
+#ifndef CHARACTER_HPP
+#define CHARACTER_HPP
+
+#include <string>
+
+#include "Util/GameObject.hpp"
+
+class Character : public Util::GameObject {
+public:
+    Character() : Character(GA_RESOURCE_DIR"/gray.bmp") {};
+
+    explicit Character(const std::string& ImagePath);
+
+    Character(const Character&) = delete;
+
+    Character(Character&&) = delete;
+
+    Character& operator=(const Character&) = delete;
+
+    Character& operator=(Character&&) = delete;
+
+    [[nodiscard]] const std::string& GetImagePath() const { return m_ImagePath; }
+
+    [[nodiscard]] const glm::vec2& GetPosition() const { return m_Transform.translation; }
+
+    [[nodiscard]] bool GetVisibility() const { return m_Visible; }
+
+    void SetImage(const std::string& ImagePath);
+
+    void SetPosition(const glm::vec2& Position) { m_Transform.translation = Position; }
+
+    void ResetPosition() { m_Transform.translation = {.5f, .5f}; }
+
+     [[nodiscard]] bool IfCollides(const Character& other) const {(void)other; return false;} // TODO: Implement the collision detection
+
+    // TODO: Add and implement more methods and properties as needed to finish Giraffe Adventure.
+
+private:
+    std::string m_ImagePath;
+};
+
+
+#endif //CHARACTER_HPP
