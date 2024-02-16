bd3d5da4ca4d46c341b3043554fc1a290e9246ea
DF <dragonflybaby2.0@gmail.com>
2024-02-16 17:17:40 +0800

    Enhance: Add phase resources classes.
    - Add BackgroundImage, TaskText, and PhaseManager.

--- include/TaskText.hpp
+++ include/TaskText.hpp
@@ -3,21 +3,22 @@
 
 #include "Util/GameObject.hpp"
 #include "Util/Text.hpp"
+#include "Util/Color.hpp"
 
 class TaskText : public Util::GameObject {
 public:
     TaskText() : GameObject(
-            std::make_unique<Util::Text>(GA_RESOURCE_DIR"/Inkfree.ttf", 20,
-                                         append_string_views(s_PhaseTasks[0], s_Validation)),
+            std::make_unique<Util::Text>(GA_RESOURCE_DIR"/Font/Inkfree.ttf", 20,
+                                         append_string_views(s_PhaseTasks[0], s_Validation),
+                                         Util::Color(1.0F, 1.0F, 1.0F, 1.0F)),
             100) {
         m_Transform.translation = {0.0F, -270.F};
     }
 
-    void NextPhase() {
+    void NextPhase(const int phase) {
         auto temp = std::dynamic_pointer_cast<Util::Text>(m_Drawable);
-        temp->SetText(append_string_views(s_PhaseTasks[++m_Phase], s_Validation));
+        temp->SetText(append_string_views(s_PhaseTasks[phase], s_Validation));
     }
-
 private:
     inline static std::string append_string_views(std::string_view sv1, std::string_view sv2) {
         return std::string(sv1) + "\n" + std::string(sv2);
@@ -32,8 +33,6 @@ private:
             "Design a program to countdown, stop animation after OK display"
     };
     static constexpr std::string_view s_Validation = "Press Enter to validate";
-
-    uint8_t m_Phase = 0;
 };
 
 
---
+++ src/PhaseManager.cpp
@@ -0,0 +1,12 @@
+#include "PhaseResourceManger.hpp"
+
+PhaseResourceManger::PhaseResourceManger() {
+    m_TaskText = std::make_shared<TaskText>();
+    m_Background = std::make_shared<BackgroundImage>();
+}
+
+void PhaseResourceManger::NextPhase() {
+    if (m_Phase == 13) return;
+    m_Background->NextPhase(m_Phase);
+    m_TaskText->NextPhase(m_Phase++ / 2);
+}
