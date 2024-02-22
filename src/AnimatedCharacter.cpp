faac78cb3770a425ed48b29dd66576626bf474ea
df <dragonflybaby2.0@gmail.com>
2024-02-22 19:36:54 +0800

    Adapt: Update the GA to fit in updated PTSD.
    - Update the parameter constructing task text.
    - Update the method used validating if the animation has ended.

--- src/AnimatedCharacter.cpp
+++ src/AnimatedCharacter.cpp
@@ -2,13 +2,10 @@
 
 
 AnimatedCharacter::AnimatedCharacter(const std::vector<std::string>& AnimationPaths) {
-    m_Drawable = std::make_shared<Util::Animation>(AnimationPaths, 100, false, 0);
-    auto temp = std::dynamic_pointer_cast<Util::Animation>(m_Drawable);
-    temp->SetLooping(true);
+    m_Drawable = std::make_shared<Util::Animation>(AnimationPaths, false, 500, true, 0);
 }
 
 bool AnimatedCharacter::IfAnimationEnds() const {
-//    auto animation = std::dynamic_pointer_cast<std::shared_ptr<Util::Animation>>(m_Drawable)->get();
-//        return animation->GetCurrentIndex() == animation->GetFrames().size() - 1;
-    return true;
+    auto animation = std::dynamic_pointer_cast<std::shared_ptr<Util::Animation>>(m_Drawable)->get();
+        return animation->GetCurrentFrameIndex() == animation->GetFrameCount() - 1;
 }
